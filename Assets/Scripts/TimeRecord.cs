d2e868aa49c91907f93fc6acc529feb4565226d4
Manuel Rauber <manuel.rauber@thinktecture.com>
2020-07-11 09:07:40 +0200

    chore: initial project

---
+++ Assets/Scripts/TimeRecord.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace BoundfoxStudios.TimeManipulation
+{
+  public struct TimeRecord
+  {
+    public Vector3 Position;
+    public Quaternion Rotation;
+    public Vector3 Velocity;
+    public Vector3 AngularVelocity;
+  }
+}
